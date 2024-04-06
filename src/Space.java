fa255aeb2579d98b27b506fe8a705ea90d0d241c
mi-nadir <vipok72@yandex.ru>
2024-04-06 13:16:47 +0300

    Equals

---
+++ src/Space.java
@@ -0,0 +1,11 @@
+public class Space {
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = "Hello";
+        if(first.equals(second)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Not OK");
+        }
+    }
+}
